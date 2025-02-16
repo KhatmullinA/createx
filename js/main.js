94c573a2b4020d40961c97c215662fc23a1f45b2
KhatmullinA <khatmullin.artur@mail.ru>
2025-02-16 20:16:56 +0500

    create testimonials section

--- js/main.js
+++ js/main.js
@@ -4,4 +4,34 @@ $(function () {
   $('.directions__filter-btn').on('click', function () { 
     $(this).addClass('directions__filter-btn--active')
   })
+
+  $('.team__slider').slick({
+    arrows: false,
+    slidesToShow: 4,
+    Infinity: true,
+    draggable: false,
+  })
+  $('.team__slider-prev').on('click', function (e) { 
+    e.preventDefault();
+    $('.team__slider').slick('slickPrev')
+  })
+  $('.team__slider-next').on('click', function (e) { 
+    e.preventDefault();
+    $('.team__slider').slick('slickNext')
+  })
+  $('.testimonials__slider').slick({
+    arrows: false,
+    dots: true,
+    appendDots: $('.testimonials__dots'),
+  })
+
+  $('.testimonials__prev').on('click', function (e) {
+    e.preventDefault();
+    $('.testimonials__slider').slick('slickPrev')
+  })
+  $('.testimonials__next').on('click', function (e) {
+    e.preventDefault();
+    $('.testimonials__slider').slick('slickNext')
+  })
+
 })
